4e66cf01d784c76214cf05a1bc7a01ee505ce0b9
sabeom <tkqja717@naver.com>
2024-12-25 17:42:13 +0900

    initial commit

---
+++ src/main/java/com/example/AFDM/AfdmApplication.java
@@ -0,0 +1,13 @@
+package com.example.AFDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AFDM/AfdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AFDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
